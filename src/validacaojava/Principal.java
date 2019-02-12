08d33e38326c6085bc682edfd94e32bc3df91615
Aluno <Aluno@SN-279229>
2019-02-12 21:34:22 -0200

    Trabalho validacao

--- src/validacaojava/Principal.java
+++ src/validacaojava/Principal.java
@@ -7,11 +7,11 @@ import javax.swing.JFrame;
 public class Principal {
     public static void main(String[] args){
         JFrame janela = new JFrame("Carlos");
-        Validacao painel = new Validacao();
+        Painell painel = new Painell();
         
         //Definir atributos de tela
         janela.setDefaultCloseOperation(janela.EXIT_ON_CLOSE);
-        janela.setSize(600, 400);
+        janela.setSize(800, 600);
         janela.add(painel);
         janela.setVisible(true);
         
